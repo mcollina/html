eaf33b19b58d9cd64185b2a748d61aba9409c1e0
Gürgün Dayıoğlu <gurgun.dayioglu@icloud.com>
2023-09-20 18:56:25 +0200

    make `html` imperative (#11)
    * use switch
    
    * add node_modules to .gitignore
    
    * move --length out of the switch
    
    * don't use endsWith to check a single char

--- html.js
+++ html.js
@@ -5,6 +5,7 @@ const escapeChars = {
   "<": "&lt;",
   ">": "&gt;",
 };
+
 const escapeRegExp = new RegExp(`[${Object.keys(escapeChars).join("")}]`, "gv");
 
 /**
@@ -12,21 +13,38 @@ const escapeRegExp = new RegExp(`[${Object.keys(escapeChars).join("")}]`, "gv");
  * @param  {...any} expressions
  * @returns {string}
  */
-const html = ({ raw: literals }, ...expressions) =>
-  literals.reduce((acc, lit, i) => {
-    --i;
+const html = ({ raw: literals }, ...expressions) => {
+  let length = literals.length;
 
+  switch (length) {
+    case 0:
+      return "";
+    case 1:
+      return literals[0];
+  }
+
+  --length;
+
+  let acc = "";
+
+  for (let i = 0; i < length; ++i) {
+    let lit = literals[i];
     let str = Array.isArray(expressions[i])
       ? expressions[i].join("")
       : expressions[i]?.toString() ?? "";
 
-    if (literals[i] && literals[i].endsWith("!")) {
-      acc = acc.slice(0, -1);
+    if (lit && lit[lit.length - 1] === "!") {
+      lit = lit.slice(0, -1);
     } else if (str) {
       str = str.replace(escapeRegExp, (match) => escapeChars[match]);
     }
 
-    return acc + str + lit;
-  });
+    acc += lit += str;
+  }
+
+  acc += literals[length];
+
+  return acc;
+};
 
 export { html };
