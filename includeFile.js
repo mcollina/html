f9a03bf9ad814768eba457df208e0474d0325d29
Gürgün Dayıoğlu <gurgun.dayioglu@icloud.com>
2023-09-20 20:01:09 +0200

    pass string directly (#13)

--- includeFile.js
+++ includeFile.js
@@ -10,7 +10,7 @@ const includeFile = (path) => {
   let file = fileCache.get(path);
 
   if (file === undefined) {
-    file = readFileSync(path, { encoding: "utf8" });
+    file = readFileSync(path, "utf8");
     fileCache.set(path, file);
   }
 
